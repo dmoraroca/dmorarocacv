62ec35e0b98c61ea298980ed8f1aec42bbb197bc
DMR <dmoraroca@gmail.com>
2019-06-22 21:55:28 +0200

    solo index.html

--- dist/server.js
+++ dist/server.js
@@ -9,7 +9,7 @@ const app = express();
 app.use(express.static(__dirname + '/dist'));
 
 app.get('/*', function(req,res) {  
-    //res.sendFile(path.join(__dirname+'/dist/index.html'));   
+    res.sendFile(path.join(__dirname+'./index.html'));   
 	res.send("adfasd");
 });  
 
