b32825d45b89236a6194b5d81886ddefb6ebaf87
DMR <dmoraroca@gmail.com>
2019-06-22 23:31:20 +0200

    lock changed

--- dist/server.js
+++ dist/server.js
@@ -5,12 +5,12 @@ const path = require('path');
 
 const app = express();   
 
-// Serve only the static files form the dist directory    
-//app.use(express.static('./dist'));
+app.use('./dist', express.static(path.join(__dirname, '/')));
 
-app.get('/', function(req,res) {  
-    res.sendFile(path.join('index.html'));   
-});  
+// Handle React routing, return all requests to React app
+app.get('*', function(req, res) {
+	res.sendFile(path.join(__dirname, './dist', 'index.html'));
+});
 
 // Start the app by listening on the default Heroku port    
 app.listen(process.env.PORT || 8080);
